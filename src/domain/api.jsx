85d27a0197aa12fbd20525e6a4344086c179f0ac
mdaffazw45 <andafsul4@gmail.com>
2023-11-03 16:24:59 +0700

    form , alert , history

--- src/domain/api.jsx
+++ src/domain/api.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import moment from "moment";
 
 export const callAPI = async (endpoint, method, headers, params, data) => {
   const baseURL = "http://localhost:3000/absent";
@@ -14,3 +15,21 @@ export const callAPI = async (endpoint, method, headers, params, data) => {
   const response = await axios(options);
   return response?.data;
 };
+
+
+export async function postDataToServer(data) {
+  try {
+    // Generate the current time in your specified format
+    const checkin = moment().format('YYYY-MM-DDTHH:mm:ss');
+
+    // Update the "checkin" field in the data object
+    data.checkin = checkin;
+
+    const response = await axios.post('http://localhost:3000/absent', data);
+    console.log('Response from the server:', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error posting data:', error);
+    throw error;
+  }
+}
--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -1,9 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import '../styles/form.css'
+import { postDataToServer } from "../domain/api";
 
 function Form() {
+
+  const [absents, setAbsents] = useState([]);
+  const [forms , setForms] = useState({
+    id : "",
+    nama : "",
+    checkin : "",
+    checkout : "",
+    status :"",
+  })
+
+
+  const handleFormChange = (event) => {
+    const { name, value } = event.target;
+    
+    const updatedForms = {...forms}
+
+    if (status === 'checkin') {
+      const checkin = moment().format('YYYY-MM-DDTHH:mm:ss');
+      updatedForms[status] = checkin;
+    } else {
+      updatedForms[name] = value;
+    }
+
+    setForms(updatedForms);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    
+         // Check if the password is less than 6 characters
+
+    try {
+      const data = {
+        id : forms.id,
+        nama : forms.nama,
+        checkin : forms.checkin,
+        checkout :  forms.checkout,
+        status : 'checkin',
+      };
+      const response = await postDataToServer(data);
+      console.log(response, "Hasil Response");
+  
+      // Update the posts state with the new data
+      setAbsents((prevAbsents) => [...prevAbsents, response]);
+  
+      // Clear the form input fields
+      setForms({
+        nama: "",
+        checkin: "",
+        checkout: "",
+        category: "",
+        status: "",
+      });
+
+      alert("Absensi Berhasil Bisa Ditambah");
+
+    } catch (error) {
+      console.log('Error Posting Data', error);
+    }
+  }
+  
+  
+  ;
+
   return (
-    <div className="form">
-      <h1>form</h1>
+    <div className="form-konten">
+      <div className="header-judul">
+        <h1>Isi Daftar Absensi</h1>
+      </div>
+      <div className="konten-isi">
+      <div className="mengisi-formulir">
+            <form  onSubmit={handleFormSubmit}>
+              <div className="input-container">
+                <div className="label-name">Nama: </div>
+                <input
+                  type="text"
+                  name="nama"
+                  value={forms.nama}
+                  placeholder="nama"
+                  onChange={handleFormChange}
+                />
+              </div>
+              <Button variant="outlined" type="submit" className="button">
+                Submit
+              </Button>
+            </form>
+            </div>
+      </div>
     </div>
   );
 }
